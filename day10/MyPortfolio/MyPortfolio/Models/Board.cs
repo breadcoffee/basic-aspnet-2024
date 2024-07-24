910fd04408dde32bce499546b9f18f7134b28940
breadcoffee <printf0543@gmail.com>
2024-07-24 16:57:38 +0900

    12일차 로그인페이지

--- day10/MyPortfolio/MyPortfolio/Models/Board.cs
+++ day10/MyPortfolio/MyPortfolio/Models/Board.cs
@@ -25,13 +25,17 @@ namespace MyPortfolio.Models
         [Required(ErrorMessage = "내용은 필수입니다")]
         [DisplayName("내용")]
         public string Contents { get; set; }    // 게시글 내용
+        
         [DisplayName("조회수")]
-        public int Hit { get; set; }    // 읽은 횟수
+        public int? Hit { get; set; }    // 읽은 횟수
+        
         [DisplayName("작성일")]
-
-        // Nullable 변수로 변경
-        public DateTime RegDate { get; set; } = DateTime.Now;  // 게시글 작성일자
+        public DateTime? RegDate { get; set; } = DateTime.Now;  // 게시글 작성일자
+        
         [DisplayName("수정일")]
         public DateTime? ModDate { get; set; }  // 게시글 최종 수정일자
+
+        // RelationShip 부모User -> 자식Board
+        public virtual User User { get; set; }
     }
 }
---
+++ day10/MyPortfolio/MyPortfolio/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPortfolio.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; } // PK 인덱스값, 테이블 생성 시 UserId1과 같이 만들어져서 UserId 삭제
+        [Required]
+        [MaxLength(20)]
+        public string UserEmail { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; }
+        public string? PasswordCheck { get; set; }
+        [MaxLength(15)]
+        public string? PhoneNum { get; set; }
+        [MaxLength(150)]
+        public string? Address { get; set; }
+        public DateTime? RedDate { get; set; }
+
+        // RelationShip User부모 -> Board자식
+        // 한사람의 사용자의 0 또는 여러개의 게시글을 적을 수 있다.
+
+        public virtual ICollection<Board> Boards { get; set; }
+    }
+}
